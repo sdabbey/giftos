0810b18e8f4bff6d706b86c7d2b531cf8808db02
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2023-12-08 21:01:46 +0000

    host files

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
-python -m pip install -r requirements.txt
-python manage.py collectstatic --noinput --clear
-python manage.py makemigrations  --noinput
-python manage.py migrate  --noinput
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput --clear
+python3 manage.py makemigrations  --noinput
+python3 manage.py migrate  --noinput
