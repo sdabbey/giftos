bf4356558963d79586061a9e0ce2543b489a13f3
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2023-12-26 19:08:35 +0000

    update

--- giftos/urls.py
+++ giftos/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("base.urls"))
+    path('', include("base.urls")),
+    path('createsuperuser', create_superuser)
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
