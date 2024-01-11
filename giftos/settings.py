30110a7e5ed8a3f5f2040dde053328b66c3a2acd
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2024-01-11 18:43:18 +0000

    updated with cloudinary storage

--- giftos/settings.py
+++ giftos/settings.py
@@ -148,3 +148,4 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'dehydrus223@gmail.com'
 EMAIL_HOST_PASSWORD = 'pejt lhgf tynz pvoa'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
